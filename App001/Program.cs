3d6c055da264df992de2ac7878a8b56646e43ea2
pablovass <faktory.fm@live.com>
2021-05-08 17:58:43 -0300

    1mer programa

---
+++ App001/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace App001
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine(" hola pablo vallejos!");
+        }
+    }
+}
