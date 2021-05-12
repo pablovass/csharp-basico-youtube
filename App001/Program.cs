966bdd9546b1660d90447feb8d5ec20f3557844c
pablovass <faktory.fm@live.com>
2021-05-12 20:15:44 -0300

    dato de entrada y salida

--- App001/Program.cs
+++ App001/Program.cs
@@ -5,22 +5,22 @@ namespace App001
 {
     class Program
     {
-        //float tipoFlotante;
         
+        // datos de entrada y salida 
+        // como crear una algoritmos 
+        // que es variable
         static void Main(string[] args)
         {
-            
-            // programa que me deje escribir cualquier cosa
-            
-            // incio 
-            WriteLine("podes escribir cualquier cosa");
-            string palabras;
-            
-            // problema ==  proseso 
-            palabras=ReadLine(); //aca va estar almasenando todo lo de consola 
-
-            //salida  
-             WriteLine("usted escribio: "+ palabras);   
+            //hacer un programa que me deje escribir y guardar unas palabras
+           
+            //inicio
+             string palabra;
+             WriteLine("escriba lo que usted quiera");
+            //problema a resolver 
+            // proceso
+            palabra = ReadLine();
+            //fin / salida 
+            WriteLine("usted escribio: " + palabra);
 
         }
     }
